1e4122e2ec5d7561d88689f33ba2f5e984940c18
mwichigi <ngangamj828@gmail.com>
2026-04-08 17:49:04 +0300

    test bare minimum

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,19 +1,3 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/auth', require('../routes/auth'));
-app.use('/api/products', require('../routes/products'));
-app.use('/api/orders', require('../routes/orders'));
-app.use('/api/payments', require('../routes/payments'));
-
-app.get("/api/test", (req, res) => {
+module.exports = (req, res) => {
   res.json({ message: "Backend working!" });
-});
-
-module.exports = app;
+};
